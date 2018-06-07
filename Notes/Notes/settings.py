f6d36960c2f1bbe090bc9093559a5e7228c86408
SashaZag <dangercloselp@gmail.com>
2018-06-07 16:04:56 +0300

    DB changet to PSQL

--- Notes/Notes/settings.py
+++ Notes/Notes/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'i220872',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
